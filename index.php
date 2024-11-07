65d3034d929f313e9884281384766508955f9e5c
imkarlch <145731086+imkarlch@users.noreply.github.com>
2024-11-06 19:32:43 -1200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<title>My First Ubuntu Server Php Development</title>
+</head>
+<body>
+<center>
+	<p>My First Ubuntu Server Php Development</p> <br>
+
+	<div> 
+		<label>Firstname:</label>
+		<input type="text" name="firstname">
+	</div><br>
+
+	<div> 
+		<label>Middlename:</label>
+		<input type="text" name="firstname">
+	</div><br>
+
+	<div> 
+		<label>Lastname:</label>
+		<input type="text" name="lastname">
+	</div><br>
+
+	<div> 
+		<label>Age:</label>
+		<input type="text" name="age">
+	</div><br>
+
+	<div> 
+		<label>Address:</label>
+		<input type="text" name="address">
+	</div><br>
+
+	<div> 
+		<label>Course and Section:</label>
+		<input type="text" name="course">
+	</div><br>
+
+	<div> 
+		<label>Submit:</label>
+		<input type="submit" name="course">
+	</div><br>
+
+</center>
+</body>
+</html>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
